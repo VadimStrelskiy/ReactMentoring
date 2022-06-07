6fbf49c8eb8aba739707ab122918d6d6888a4c01
Vadzim Strelski <Vadzim_Strelski@epam.com>
2022-06-07 21:23:03 +0300

    Home task 4

--- src/Components/Body/MovieCard/MovieCard.tsx
+++ src/Components/Body/MovieCard/MovieCard.tsx
@@ -4,6 +4,8 @@ import {useState} from 'react';
 import {Movie} from '../../App';
 import {DeleteModal} from '../../Modals/DeleteModal/DeleteModal';
 import {EditModal} from '../../Modals/EditModal/EditModal';
+import {ContextMenu, ContextMenuElement} from '../../Common/ContextMenu/ContextMenu';
+
 
 interface MovieCardProps{
   movie: Movie,
@@ -11,7 +13,6 @@ interface MovieCardProps{
 }
 
 export function MovieCard({movie, onDelete}: MovieCardProps) {
-  const [menuOpenState, setMenuOpen] = useState(false);
   const [editOpen, setEditOpen] = useState(false);
   const [deleteOpen, setDeleteOpen] = useState(false);
 
@@ -19,25 +20,31 @@ export function MovieCard({movie, onDelete}: MovieCardProps) {
     onDelete(movie.id);
   }
 
+  const elements : ContextMenuElement[] = [
+    {
+      label: 'Edit',
+      onClick: () => setEditOpen(true),
+    },
+    {
+      label: 'Delete',
+      onClick: () => setDeleteOpen(true),
+    },
+  ];
+
   return (
     <div className='movie-card-container'>
       <img className='movie-image' src={`/static/${movie.image}`}/>
       <p className='movie-title'>{movie.title}</p>
       <p className='movie-year'>{movie.date.getFullYear()}</p>
       <p className='movie-genre'>{movie.genre}</p>
-      <Popup arrow={false} trigger={
-        <button className='movie-menu-button'>
-          <div className='movie-menu-button-dot first'/>
-          <div className='movie-menu-button-dot'/>
-          <div className='movie-menu-button-dot'/>
-        </button>}
-      open={menuOpenState} onOpen={() => setMenuOpen(true)}>
-        <div className="movie-menu">
-          <button className='movie-menu-close' onClick={() => setMenuOpen(false)}>&times;</button>
-          <div className='movie-menu-edit' onClick={() => setEditOpen(true)}>Edit</div>
-          <div className='movie-menu-delete' onClick={() => setDeleteOpen(true)}>Delete</div>
-        </div>
-      </Popup>
+      <ContextMenu trigger={<svg className='movie-menu-button'>
+        <circle fill="#2A202D" r="18px" cx="18px" cy="18px"/>
+        <circle fill="white" r="2px" cx="18px" cy="10px"/>
+        <circle fill="white" r="2px" cx="18px" cy="18px"/>
+        <circle fill="white" r="2px" cx="18px" cy="26px"/>
+      </svg>}
+      elements = {elements}/>
+
       <Popup modal closeOnDocumentClick={false} open={editOpen} onClose={() => setEditOpen(false)}><EditModal onClose={() => setEditOpen(false)} movie={movie}/></Popup>
       <Popup modal closeOnDocumentClick={false} open={deleteOpen} onClose={() => setDeleteOpen(false)}>{<DeleteModal onDelete={onDeleteId} onClose={() => setDeleteOpen(false)}/>}</Popup>
     </div>
---
+++ src/Components/Common/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,35 @@
+import './ContextMenu.scss';
+
+import {useState} from 'react';
+import Popup from 'reactjs-popup';
+
+export interface ContextMenuElement{
+    label: string,
+    onClick: () => void
+}
+
+interface ContextMenuProps{
+    trigger: JSX.Element,
+    elements: ContextMenuElement[]
+}
+
+export function ContextMenu({trigger, elements} : ContextMenuProps) {
+  const [menuOpenState, setMenuOpen] = useState(false);
+
+  return (
+    <Popup arrow={false} trigger={trigger} open={menuOpenState} onOpen={() => setMenuOpen(true)}>
+      <div className="context-menu">
+        <button className='context-menu-close' onClick={() => setMenuOpen(false)}>&times;</button>
+        <br/>
+        <ul>
+          {
+            elements.map((element) =>
+              (
+                <li key={element.label} onClick={element.onClick}>{element.label}</li>
+              ),
+            )}
+        </ul>
+      </div>
+    </Popup>
+  );
+}
